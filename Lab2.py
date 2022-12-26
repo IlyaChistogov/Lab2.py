212cf85d76a100c9a8cdebf9e3532e93937b70b3
IlyaChistogov <115698573+IlyaChistogov@users.noreply.github.com>
2022-12-26 10:49:20 +0500

    Вывод запрашиваемых данных

--- Lab2.py
+++ Lab2.py
@@ -13,3 +13,27 @@ def read(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
 
+class Birthday:
+    def __init__(self):
+        self.year = randint(2000, 2022)
+        self.month = choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+        self.day = randint(1, 31)
+
+data = {
+    'Birthdays': []
+}
+
+for i in range(50):
+    data['Birthdays'].append(Birthday().__dict__)
+
+#write(data, 'data1.json')
+
+n_data = read('data1.json')
+
+print(n_data['Birthdays'][49])
+
+g = Birthday()
+g.name = n_data['Birthdays'][0]['month']
+
+print(g.name)
+input()
