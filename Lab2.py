7d6ef1a947ede82024033400f17e4658969af469
IlyaChistogov <115698573+IlyaChistogov@users.noreply.github.com>
2022-12-26 10:46:32 +0500

    начало

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+import json
+from random import randint, choice
+from pprint import pprint
+
+def write(data, filename):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2)
+
+
